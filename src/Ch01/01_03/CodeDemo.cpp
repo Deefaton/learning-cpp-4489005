cc00f8bff7cdfc58dae03128f031e50adaa3de90
Deefaton <132373147+Deefaton@users.noreply.github.com>
2025-11-24 18:38:40 +0000

    My code to ask and print username

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -3,12 +3,13 @@
 // Console Interaction, by Eduardo Corpeño 
 
 #include <iostream>
-#include <string>
+#include <string> //library with cin
 
 int main(){
-    std::string str;
-    std::cin >> str;
-    std::cout << str;
+    std::string str; //string variable declaration
+    std::cout << "What is your name? " << std::flush; // flush makes sure the output is sent to the terminal before waiting for user input
+    std::cin >> str; // capture user input
+    std::cout << "Hello " << str << "!" << std::endl; // Hello message with user input
 
     std::cout << std::endl << std::endl;
     return (0);
