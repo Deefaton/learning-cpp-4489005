7e76e7df00edb6314ba6127e83f9eed9770115b6
Deefaton <132373147+Deefaton@users.noreply.github.com>
2025-11-24 18:21:43 +0000

    Written "hi there" code

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,20 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+// The first line includes the libraries we will use, this is a pre-processor directive. Here we use iostream which is part of the C++ standard library. 
+// iostream contains objects and functions to print text to the screen and recieve text from keyboard
+#include <iostream>
+// Create main function which returns an integer
+int main() {
+    // using object std from iostream, membership to standard library represented by the "scope resolution operator" the double colon
+    // the object is called "cout" which is short for character output - it represents the character window - we need to send text to it
+    // We can send things consecutively, here we send a new line character as well "endl"
+    std::cout << "Hi there!" << std::endl;
+
+
+    std::cout << std::endl << std::endl;
+    //The return value - the main function returns an integer
+    // Use 0 which traditionally means executed without errors
+    return(0);
+
+}
