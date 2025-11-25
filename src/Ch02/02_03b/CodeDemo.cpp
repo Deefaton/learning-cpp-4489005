546e4bde2cd807b2de126d9d963ff03cf8c6c1e9
Deefaton <132373147+Deefaton@users.noreply.github.com>
2025-11-25 08:17:29 +0000

    Update

--- src/Ch02/02_03b/CodeDemo.cpp
+++ src/Ch02/02_03b/CodeDemo.cpp
@@ -4,9 +4,23 @@
 
 #include <iostream>
 
+// Notice the variables are declared on the same indentation as the main function - this means a and b are global variables and therefore accessible to all parts of the code.
+int a, b = 5; //can declare several variables in one line here a is not yet assigned, b is assigned 5
 int main(){
-    std::cout << "Hi There!" << std::endl;
-    
+    bool my_flag;
+    a = 7; // assign variables
+    my_flag = false;
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "flag = " << my_flag << std::endl;
+    my_flag = true;
+    std::cout << "flag = " << my_flag << std::endl;
+    std::cout << "a + b = " << a + b << std::endl;
+    std::cout << "b - a = " << b - a << std::endl;
+    unsigned int positive;
+    positive = b - a;
+    std::cout << "b - a (unsigned) = " << positive << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
